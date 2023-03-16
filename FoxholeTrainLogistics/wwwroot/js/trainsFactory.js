acd1a87dd6bd0f40cb157bfca78efb9765e40298
Nathan <67047470+nathnado22@users.noreply.github.com>
2023-03-17 03:09:13 +1100

    Working on TrainsFactory implementation
    Working on a factory to dynamically create and delete train cars

---
+++ FoxholeTrainLogistics/wwwroot/js/trainsFactory.js
@@ -0,0 +1,35 @@
+﻿// Train Creation Factory
+function createTrainCar(trainCarType) {
+    const trainContainer = document.getElementById("train-container");
+
+    if (trainContainer === null)
+        throw new DOMException("Unable to locate trainContainer - was this script ran before the trainContainer was created?");
+
+    const btn = trainContainer.appendChild(document.createElement("button"));
+    btn.className = "ftl-interact ftl-button";
+    btn.onclick = () => trainContainer.removeChild(btn);
+
+    console.log('adding ' + trainCarType);
+
+    var img = undefined;
+    switch (trainCarType) {
+        case "Engine": {
+            img = btn.appendChild(document.createElement("img"));
+            img.src = "./img/trainBlack_side.png";
+            break;
+        }
+        case "CoalCar": {
+            img = btn.appendChild(document.createElement("img"));
+            img.src = "./img/coalCarBlack_side.png";
+            break;
+        }
+        default:
+            throw new DOMException("Invalid train car type \"" + trainCarType + "\"");
+    }
+
+    img.className = "ftl-icon";
+}
+function removeTrainCar(id) {
+    const trainContainer = document.getElementById("train-container");
+
+}
