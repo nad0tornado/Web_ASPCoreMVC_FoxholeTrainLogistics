63861972323d4d8cac17c2a651fa462910c6a94e
Nathan <nlinsley@nadocointeractive.com>
2023-04-16 23:58:02 +1000

    Working on Selection

--- FoxholeTrainLogistics/wwwroot/js/train.js
+++ FoxholeTrainLogistics/wwwroot/js/train.js
@@ -41,7 +41,7 @@ function Train(_train, isInteractable=false) {
 
     const getFlatbedCars = () => this.train.Cars.filter(c => c.Type === "FlatbedCar");
 
-    const LoadItem = (item) => {
+    const AddItem = (item) => {
         if (!item)
             throw new DOMException("item cannot be null");
 
@@ -51,14 +51,14 @@ function Train(_train, isInteractable=false) {
         var updatedFlatcar = undefined;
 
         if (isMPFOrSinglePackage)
-            updatedFlatcar = loadPackagedItem(item);
+            updatedFlatcar = addPackagedItem(item);
         else
-            updatedFlatcar = loadMultiContainerItem(item);
+            updatedFlatcar = addMultiContainerItem(item);
 
         console.log("[Train] Updated Flat Car:", updatedFlatcar);
     }
 
-    const loadPackagedItem = (item) => {
+    const addPackagedItem = (item) => {
         const flatbedCars = getFlatbedCars();
         const flatbedCarsWithoutContainer = flatbedCars.filter(fb => fb.Container == null);
 
@@ -88,38 +88,66 @@ function Train(_train, isInteractable=false) {
         }
     }
 
-    const loadMultiContainerItem = (item) => {
+    const addMultiContainerItem = (item) => {
         const flatbedCars = getFlatbedCars();
         const flatbedCarsWithRightContainer = flatbedCars.filter(fb => fb.Container && fb.Container.Type === item.ShippingType);
         const flatbedCarsWithFreeSpace = flatbedCarsWithRightContainer.filter(fb => fb.Container.Contents.length < fb.Container.Capacity);
         const flatbedCarsWithoutContainer = flatbedCars.filter(fb => fb.Container == null);
 
+        var updatedFlatcar = undefined;
         if (flatbedCarsWithFreeSpace.length > 0) {
             const flatbedWithFreeSpace = flatbedCarsWithFreeSpace[0];
             flatbedWithFreeSpace.Container.Contents.push(item);
 
-            return flatbedWithFreeSpace;
+            updatedFlatcar = flatbedWithFreeSpace;
         }
         else if (flatbedCarsWithoutContainer.length > 0) {
             const newContainer = { ...MultiItemContainerTemplates[item.ShippingType] };
             newContainer.Contents.push(item);
-            //newContainer.tooltip = 
 
             const flatCar = flatbedCarsWithoutContainer[0];
             flatCar.Container = newContainer;
             trainsFactory.addContainerToFlatcar(flatCar, newContainer);
 
-            return flatCar;
+            updatedFlatcar = flatCar;
         }
         else if (flatbedCarsWithFreeSpace.length === 0) {
             const newContainer = { ...MultiItemContainerTemplates[item.ShippingType] };
             newContainer.Contents.push(item);
 
             const newFlatcar = { ...TrainCarTemplates.FlatbedCar, Container: newContainer };
-            return AddTrainCar(newFlatcar);
+            updatedFlatcar = AddTrainCar(newFlatcar);
         }
+
+        updatedFlatcar.Container.Contents = updatedFlatcar.Container.Contents.sort((a, b) => {
+            if (a.DisplayName < b.DisplayName) {
+                return -1;
+            }
+            if (a.DisplayName > b.DisplayName) {
+                return 1;
+            }
+            return 0;
+        });
+        return updatedFlatcar;
     }
 
+    const RemoveItem = (item, flatcar) => {
+        if (item === undefined)
+            throw new DOMException("item cannot be undefined");
+        if (flatcar === undefined)
+            throw new DOMException("flatcar cannot be undefined");
+
+        const flatcarContainer = flatcar.Container;
+
+        if (flatcarContainer === undefined)
+            throw new DOMException("The given flatcar doesn't have a container");
+        if (!flatcarContainer.Contents.some(i => i.Type === item.Type))
+            throw new DOMException("No item of the given type exists in this flatbed car's container");
+
+        const itemToRemoveIndex = flatcarContainer.Contents.indexOf(item);
+        flatcarContainer.Contents = flatcarContainer.Contents.filter((_, i) => i !== itemToRemoveIndex);
+        
+    }
     Init();
 
     return {
@@ -127,6 +155,7 @@ function Train(_train, isInteractable=false) {
         GetTrainCar,
         AddTrainCar,
         RemoveTrainCar,
-        LoadItem
+        AddItem,
+        RemoveItem
     };
 };
