a413fafd3053d7bb9881c93626c5c26ee45b79c1
Nathan <nlinsley@nadocointeractive.com>
2023-04-20 01:23:53 +1000

    Implemented Multi Add/Delete
    + [Shipment Calculator] Holding Ctrl or Shift will add/remove x10 or x100 items to/from the train
    + Added "InputManager" to handle global input states e.g. if the Ctrl or Shift key was held down in the last frame, such as when the user intends to add/remove x10 x100 items to/from the train

---
+++ FoxholeTrainLogistics/wwwroot/js/InputManager.js
@@ -0,0 +1,27 @@
+﻿class InputManager {
+
+    static MultiplyTen = false;
+    static MultiplyHundred = false;
+
+    static Init() {
+        document.addEventListener('keydown', function ({ key }) {
+            if (key === "Control") {
+                InputManager.MultiplyTen = true;
+            }
+            else if (key === "Shift") {
+                InputManager.MultiplyHundred = true;
+            }
+        });
+
+        document.addEventListener('keyup', function ({ key }) {
+            if (key === "Control") {
+                InputManager.MultiplyTen = false;
+            }
+            if (key === "Shift") {
+                InputManager.MultiplyHundred = false;
+            }
+        });
+    }
+}
+
+InputManager.Init();
