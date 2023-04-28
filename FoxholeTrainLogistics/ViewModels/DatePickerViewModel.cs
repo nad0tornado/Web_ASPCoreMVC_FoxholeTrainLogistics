057619a0ade11186e0ac8f1a35235d9660fb038c
Nathan <nlinsley@nadocointeractive.com>
2023-04-29 01:09:14 +1000

    Copy Discord Message to Clipboard
    + Implemented copying a discord message to the clipboard which contains all information about the shipment, including each flatbed car and what is contained within

---
+++ FoxholeTrainLogistics/ViewModels/DatePickerViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace FoxholeTrainLogistics.ViewModels
+{
+    public class DatePickerViewModel
+    {
+        public Guid Id { get; set; }
+
+        public DatePickerViewModel(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
